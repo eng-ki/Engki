207fd3c5ff931cd1491ab4aebefe8c171844f7ac
Hyejin Park <triaina0@gmail.com>
2020-09-22 15:21:58 +0900

    S03P23A510-44 : Add Initial API Project

---
+++ backend/src/test/java/com/ssafy/engki/EngkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.engki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
