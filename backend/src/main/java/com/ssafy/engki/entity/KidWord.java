bae18ef250ab41691875c664c6729ffa9d6dc385
Hyejin Park <triaina0@gmail.com>
2020-09-28 02:58:56 +0900

    Add all entity classes

---
+++ backend/src/main/java/com/ssafy/engki/entity/KidWord.java
@@ -0,0 +1,29 @@
+package com.ssafy.engki.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class KidWord {
+	@EmbeddedId
+	private KidWordId id;
+
+	@Column(nullable = false, updatable = false)
+	private int count;
+
+	@Column
+	private Date recentDate;
+}
---
+++ backend/src/main/java/com/ssafy/engki/entity/Word.java
@@ -0,0 +1,31 @@
+package com.ssafy.engki.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Word {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false, updatable = false)
+	private long id;
+
+	@Column(nullable = false, updatable = false)
+	private long themeId;
+
+	@Column(nullable = false, updatable = false, length = 20)
+	private String word;
+}
