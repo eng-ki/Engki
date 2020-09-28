23504d0c4b97253054743a6b5156dafbc1312f37
Hyejin Park <triaina0@gmail.com>
2020-09-28 22:14:26 +0900

    Add MapStruct and ParentDto
    - ParentDto has specific dtos

---
+++ backend/src/test/java/com/ssafy/engki/mapper/ParentMapperTest.java
@@ -0,0 +1,22 @@
+package com.ssafy.engki.mapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.ssafy.engki.dto.ParentDto;
+import com.ssafy.engki.entity.Parent;
+
+class ParentMapperTest {
+
+	@Test
+	void toInfo() {
+		Parent parent = new Parent(123456789, "name", "email", true);
+		ParentDto.Info parentInfo = ParentMapper.INSTANCE.to(parent);
+
+		assertEquals(parentInfo.getId(), parent.getId());
+		assertEquals(parentInfo.getName(), parent.getName());
+		assertEquals(parentInfo.getEmail(), parent.getEmail());
+		assertEquals(parentInfo.isReceiveEmailFlag(), parent.isReceiveEmailFlag());
+	}
+}
