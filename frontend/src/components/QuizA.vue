4fd1fcb46649a68e7e43d561387270eba7c777eb
SONMYEONGJI <ji.exitosa@gmail.com>
2020-09-29 14:49:24 +0900

    S03P23A510-126 : Add TTS API

--- frontend/src/components/QuizA.vue
+++ frontend/src/components/QuizA.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
       <div class="quiz-img">
-      <img :src="quiz.url" @click="soundAndTranslation()"/>
+      <img :src="quiz.url" @click="soundAndTranslation(word)"/>
       <div class="quiz-text" >{{word}}</div>
+      
       </div>
   </div>
 </template>
@@ -24,7 +25,7 @@ export default {
     //api 호출
     this.quiz = {
       url: '/img/etc/puppy.jpg',
-      word_eng: 'dog',
+      word_eng: 'puppy',
       word_kor: '강아지',
     }
     this.word=this.quiz.word_eng;
@@ -47,14 +48,55 @@ export default {
       // 다했어요 버튼이 클릭됐을때 호출되는 함수, 정답이 맞으면 true, 틀리면 false 리턴
       return true
     },
-    soundAndTranslation(){
+    soundAndTranslation(word){
       this.showKorean=true;
+      speech(word);
       setTimeout(()=>{
       this.showKorean=false; 
       }, 1000);
     }
   },
 }
+
+var voices = [];
+function setVoiceList() {
+voices = window.speechSynthesis.getVoices();
+}
+setVoiceList();
+if (window.speechSynthesis.onvoiceschanged !== undefined) {
+window.speechSynthesis.onvoiceschanged = setVoiceList;
+}
+function speech(txt) {
+if(!window.speechSynthesis) {
+alert("음성 재생을 지원하지 않는 브라우저입니다. 크롬, 파이어폭스 등의 최신 브라우저를 이용하세요");
+return;
+}
+var lang = 'en-US';
+var utterThis = new SpeechSynthesisUtterance(txt);
+utterThis.onend = function (event) {
+console.log('end');
+};
+utterThis.onerror = function(event) {
+console.log('error', event);
+};
+var voiceFound = false;
+for(var i = 0; i < voices.length ; i++) {
+if(voices[i].lang.indexOf(lang) >= 0 || voices[i].lang.indexOf(lang.replace('-', '_')) >= 0) {
+utterThis.voice = voices[i];
+voiceFound = true;
+}
+}
+if(!voiceFound) {
+alert('voice not found');
+return;
+}
+utterThis.lang = lang;
+utterThis.pitch = 1;
+utterThis.rate = 1; //속도
+window.speechSynthesis.speak(utterThis);
+}
+
+
 </script>
 
 <style lang="scss" scoped>
