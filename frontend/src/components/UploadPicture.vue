325876461b2056bf04bdb7e64962c5b1502e1cd9
juhyeon <tab1200@naver.com>
2020-10-02 02:27:45 +0900

    S03P23A510-144 : Update word and sentence input ui

--- frontend/src/components/UploadPicture.vue
+++ frontend/src/components/UploadPicture.vue
@@ -4,29 +4,38 @@
       <div class="custom-edu">
         <img class="custom-img" :src="img" />
         <span class="title"
-          >아이가 학습할 문장과 단어를 커스터마이징 하세요!</span
+          >아이가 학습할 단어와 문장을 커스터마이징 하세요!</span
         >
-        <div
-          class="sentences"
-          v-for="(sentence, index) in sentences"
-          v-bind:key="index"
-        >
-          <div class="sentence" @click="selectedSentence(index)">
-            <input :value="sentence" size="34vh" :readonly="!readS" />
-          </div>
+        <span class="word" v-for="(word, index) in words" v-bind:key="index">
+          <input
+            :value="word"
+            size="7vh"
+            @click="selectedWord(index)"
+            :readonly="!readW" />
+          <img
+            src="../../public/img/icon/pencil.png"
+            :class="{ selectedpencil: selectedPencil == index }"
+            class="btn-update2"
+        /></span>
+      </div>
+
+      <div
+        class="sentences"
+        v-for="(sentence, index) in sentences"
+        v-bind:key="index"
+      >
+        <div class="sentence">
+          <img src="../../public/img/icon/copy.png" />
+
+          <input
+            @click="readS = true"
+            :value="sentence"
+            size="56vh"
+            :readonly="!readS"
+          />
         </div>
       </div>
-      <span class="word" v-for="(word, index) in words" v-bind:key="index">
-        <input
-          :value="word"
-          size="10vh"
-          @click="selectedWord(index)"
-          :readonly="!readW" />
-        <img
-          src="../../public/img/icon/pencil.png"
-          :class="{ selectedpencil: selectedPencil == index }"
-          class="btn-update2"
-      /></span>
+
       <div class="custom-start">
         <img
           v-bind="attrs"
@@ -47,96 +56,113 @@
 </template>
 <script>
 export default {
-  name: 'Report',
+  name: "Report",
   props: {
     kid: null,
   },
   data: () => {
     return {
-      img: '/img/etc/womanpuppy.jpg',
-      sentences: ['The dog and the woman are running'],
-      words: ['dog', 'woman', 'sea', 'sand'],
+      img: "/img/etc/womanpuppy.jpg",
+      sentences: ["The dog and the woman are running beside the seaside"],
+      words: ["dog", "woman", "sea"],
       isUploaded: false, // 이미지 등록시
       selectedPencil: -1,
       imageUrl: null,
       ReadS: false,
       ReadW: false,
-    }
+    };
   },
   methods: {
     test() {
       this.$swal({
         title:
           '<span style="font-family: GmarketSansMedium;font-size:1.5vw;">현재 데이터로 학습을 시작하시겠습니까?</span>',
-        type: 'warning',
+        type: "warning",
         showCancelButton: true,
-        confirmButtonText: '네',
-        cancelButtonText: '아니요',
+        confirmButtonText: "네",
+        cancelButtonText: "아니요",
         showCloseButton: true,
         showLoaderOnConfirm: true,
       }).then((result) => {
         if (result.value) {
-          this.$router.push('/quiz')
+          this.$router.push("/quiz");
         }
-      })
-    },
-    selectedSentence(index) {
-      this.readS = true
-      this.selectedCheck = index
+      });
     },
     selectedWord(index) {
-      this.readW = true
-      this.selectedPencil = index
+      this.readW = true;
+      this.selectedPencil = index;
     },
     onClickImageUpload() {
-      this.$refs.imageInput.click()
+      this.$refs.imageInput.click();
     },
     onChangeImages(e) {
-      console.log(e.target.files)
-      const file = e.target.files[0]
-      this.isUploaded = true
-      this.img = URL.createObjectURL(file)
+      console.log(e.target.files);
+      const file = e.target.files[0];
+      this.isUploaded = true;
+      this.img = URL.createObjectURL(file);
     },
   },
-}
+};
 </script>
 <style lang="scss">
-@import '../assets/sass/base.scss';
+@import "../assets/sass/base.scss";
 </style>
 <style lang="scss" scoped>
 * {
-  font-family: 'GmarketSansMedium';
+  font-family: "GmarketSansMedium";
 }
 .custom-edu {
   position: absolute;
-  left: 16.5vw;
+  top: 1vh;
+  left: 17.2vw;
   display: inline-block;
 }
 .custom-edu .custom-img {
-  width: 20vw;
+  width: 18vw;
   float: left;
-  border-radius: 1vw;
-  margin-right: 2vw;
+  border-radius: 3vw;
+  margin-right: 2.2vw;
+  border: 7px solid #f2f2f2;
 }
 
 .title {
-  font-size: 1.5vw;
+  font-size: 1.45vw;
   float: left;
+  margin-top: 1.1vh;
+  margin-bottom: 1vh;
   text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.2);
 }
 
 .sentences {
-  float: left;
-  font-size: 1.5vw;
+  position: relative;
+  top: 33vh;
+  left: 2vw;
+  font-size: 1.3vw;
   margin-top: 3vw;
   text-align: left;
 }
-
+.sentence {
+  display: inline-block;
+  // box-shadow: rgba(10, 5, 26, 0.15) 0px 48px 100px 0px;
+}
+.sentence img {
+  float: left;
+  width: 2.2vw;
+  opacity: 0.6;
+  margin-top: 1.2vw;
+  margin-left: 1vw;
+  margin-right: 0.6vw;
+}
 .sentence input {
+  float: left;
+  border-bottom: 3px dashed #f2f2f2;
   padding-bottom: 0.3vw;
   color: #4b4b4b;
   margin-right: 0.5vw;
   height: 10vh;
+  padding: 0 1vw 0 1vw;
+
   &:hover {
     opacity: 0.8;
     transition: all 0.1s;
@@ -147,77 +173,68 @@ export default {
     transition: all 0.1s;
     color: green;
   }
-  box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
-}
-// .sentence .btn-update {
-//   opacity: 0;
-//   width: 1.5vw;
-//   float: right;
-// }
-// .sentence:hover .btn-update {
-//   opacity: 1;
-//   transform: scale(3vw);
-//   float: right;
-// }
-
-// .sentence .selectedcheck {
-//   opacity: 1;
-//   transform: scale(3vw);
-//   float: right;
-// }
-
-.custom-start {
-  position: absolute;
-  right: 0vw;
-  top: 18vh;
-}
-
-.custom-start img {
-  width: 4vw;
-  &:hover {
-    opacity: 0.6;
-  }
 }
 
 .word {
-  position: relative;
-  top: 45vh;
-  left: 2vw;
   float: left;
-  margin-right: 1.5vw;
-  margin-bottom: 1.5vh;
-  padding-right: 1vw;
-  width: 10vw;
-  height: 5.5vh;
+  width: 14.5vw;
+  height: 9vh;
   /* 디자인 */
-  border-radius: 7vw;
-  background-color: #dfdfdf;
-  padding-top: 1.4vh;
+  border-radius: 10vw;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 1px 2px;
+  background-color: #f2f2f2;
+  margin-top: 4vh;
+  margin-right: 2.3vw;
+  margin-bottom: 1.5vh;
+  padding-top: 2.5vh;
+  padding-left: 1.4vw;
 
   /* 폰트 */
-  font-size: 1vw;
-  &:hover {
-    opacity: 0.8;
-  }
+  font-size: 1.5vw;
 }
 
 .word input {
   color: #4b4b4b;
+  text-align: center;
+  vertical-align: middle;
+  &:focus {
+    color: green;
+    opacity: 0.8;
+  }
 }
 .word .btn-update2 {
   opacity: 0;
   width: 1vw;
   float: right;
+  margin-top: 1vh;
+  margin-right: 1vw;
 }
 .word:hover .btn-update2 {
   opacity: 1;
   transform: scale(1vw);
   float: right;
+  margin-top: 1vh;
+  margin-right: 1vw;
 }
 
 .word .selectedpencil {
   opacity: 1;
   transform: scale(1vw);
   float: right;
+  margin-top: 1vh;
+  margin-right: 1vw;
+}
+
+.custom-start {
+  position: absolute;
+  right: 2.5vw;
+  top: 17vh;
+}
+
+.custom-start img {
+  width: 5vw;
+  &:hover {
+    opacity: 0.6;
+  }
 }
 </style>
