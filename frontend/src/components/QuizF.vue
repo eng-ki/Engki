47963e976db3470382fb79e1cf89652f4c5c32cf
juhyeon <tab1200@naver.com>
2020-10-01 15:15:19 +0900

    S03P23A510-41 : Update UploadPicture component

--- frontend/src/components/QuizF.vue
+++ frontend/src/components/QuizF.vue
@@ -47,7 +47,7 @@
 import draggable from "vuedraggable";
 export default {
   components: { draggable },
-  props: ['isDone','answer'],
+  props: ["isDone", "answer"],
   data: () => {
     return {
       quiz: null,
@@ -86,48 +86,48 @@ export default {
   },
 };
 
-// var voices = []
-// function setVoiceList() {
-//   voices = window.speechSynthesis.getVoices()
-// }
-// setVoiceList()
-// if (window.speechSynthesis.onvoiceschanged !== undefined) {
-//   window.speechSynthesis.onvoiceschanged = setVoiceList
-// }
-// function speech(txt) {
-//   if (!window.speechSynthesis) {
-//     alert(
-//       '음성 재생을 지원하지 않는 브라우저입니다. 크롬, 파이어폭스 등의 최신 브라우저를 이용하세요'
-//     )
-//     return
-//   }
-//   var lang = 'en-US'
-//   var utterThis = new SpeechSynthesisUtterance(txt)
-//   utterThis.onend = function (event) {
-//     console.log('end')
-//   }
-//   utterThis.onerror = function (event) {
-//     console.log('error', event)
-//   }
-//   var voiceFound = false
-//   for (var i = 0; i < voices.length; i++) {
-//     if (
-//       voices[i].lang.indexOf(lang) >= 0 ||
-//       voices[i].lang.indexOf(lang.replace('-', '_')) >= 0
-//     ) {
-//       utterThis.voice = voices[i]
-//       voiceFound = true
-//     }
-//   }
-//   if (!voiceFound) {
-//     alert('voice not found')
-//     return
-//   }
-//   utterThis.lang = lang
-//   utterThis.pitch = 1
-//   utterThis.rate = 1 //속도
-//   window.speechSynthesis.speak(utterThis)
-// }
+var voices = [];
+function setVoiceList() {
+  voices = window.speechSynthesis.getVoices();
+}
+setVoiceList();
+if (window.speechSynthesis.onvoiceschanged !== undefined) {
+  window.speechSynthesis.onvoiceschanged = setVoiceList;
+}
+function speech(txt) {
+  if (!window.speechSynthesis) {
+    alert(
+      "음성 재생을 지원하지 않는 브라우저입니다. 크롬, 파이어폭스 등의 최신 브라우저를 이용하세요"
+    );
+    return;
+  }
+  var lang = "en-US";
+  var utterThis = new SpeechSynthesisUtterance(txt);
+  utterThis.onend = function (event) {
+    console.log("end");
+  };
+  utterThis.onerror = function (event) {
+    console.log("error", event);
+  };
+  var voiceFound = false;
+  for (var i = 0; i < voices.length; i++) {
+    if (
+      voices[i].lang.indexOf(lang) >= 0 ||
+      voices[i].lang.indexOf(lang.replace("-", "_")) >= 0
+    ) {
+      utterThis.voice = voices[i];
+      voiceFound = true;
+    }
+  }
+  if (!voiceFound) {
+    alert("voice not found");
+    return;
+  }
+  utterThis.lang = lang;
+  utterThis.pitch = 1;
+  utterThis.rate = 1; //속도
+  window.speechSynthesis.speak(utterThis);
+}
 </script>
 <style lang="scss">
 @import "../assets/sass/base.scss";
