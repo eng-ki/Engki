93d29ba83c7417f31c456fcfaf853089dabf46e9
EUNJOUNG <raemong21@cau.ac.kr>
2020-10-02 14:16:20 +0900

    S03P23A510-141 : Update Segmentation API

--- models/pyflask/app.py
+++ models/pyflask/app.py
@@ -3,8 +3,10 @@ import emotion.Emotion_Recognition as Emotion_Recognition
 import cv2
 import numpy
 import json
+import os
 # from yolact2 import yolact2.eval
 import yolact2.eval as segmetation
+from werkzeug import secure_filename  # 파일 고유이름 확인
 
 app = Flask(__name__)
 
@@ -32,7 +34,13 @@ def emotion():
 
 @app.route('/seg', methods=['POST'])
 def seg():
-    in_path = request.form['in_path']
+    filestr = request.files['files'].read()
+    parents_id = request.files['parents_id'].read()
+    uploads_dir = "/home/ubuntu/engki/data/custom/" + parents_id + "/inputs"
+    if not os.path.exists(uploads_dir):
+        os.makedirs(uploads_dir)
+    filestr.save(os.path.join(uploads_dir, secure_filename(filestr.filename)))
+    in_path = uploads_dir+"/" + filestr.filename
     # input: ~~~~/userid/input/~~~.jpg or png
     # output: save_path+"/output/"+root_list[-1][:-4]+"_"+_class+".png"
     out_path, word = segmetation.custom_segmentation(in_path)
