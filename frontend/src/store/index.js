196d73af44f6c1c0979797420298840d32d7c9b8
SONMYEONGJI <ji.exitosa@gmail.com>
2020-10-03 20:36:05 +0900

    S03P23A510-159 : Add quiz, kid info in store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,51 +1,66 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import http from '../utils/http-common.js'
-import jwt_decode from 'jwt-decode'
-Vue.use(Vuex)
+import Vue from 'vue';
+import Vuex from 'vuex';
+import http from '../utils/http-common.js';
+import jwt_decode from 'jwt-decode';
+import createPersistedState from 'vuex-persistedstate';
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     user: {},
     token: null,
+    quiz: null,
+    kid: null,
   },
   getters: {
     getUser: function (state) {
-      return state.user
+      return state.user;
     },
     getToken: function (state) {
-      return state.token
+      return state.token;
+    },
+    getQuiz: function (state) {
+      return state.quiz;
+    },
+    getKid: function (state) {
+      return state.kid;
     },
   },
   mutations: {
     setUser(state, payload) {
-      state.user = payload
+      state.user = payload;
     },
     setToken(state, payload) {
-      state.token = payload
+      state.token = payload;
+    },
+    setQuiz(state, payload) {
+      state.quiz = payload;
+    },
+    setKid(state, payload) {
+      state.kid = payload;
     },
   },
   actions: {
     kakaoLogin(context, { access_token }) {
-      console.log(access_token)
+      console.log(access_token);
 
       http
         .post('parents/login', { accessToken: access_token })
         .then(({ data }) => {
-          context.commit('setToken', data)
+          context.commit('setToken', data);
           // 'sub' 라는 key로 jwt token 디코딩해서 parents ID 받아오기
-          var token = data
-          var parent_id = jwt_decode(token).sub
+          var token = data;
+          var parent_id = jwt_decode(token).sub;
           http
             .get('parents/' + parent_id, { headers: { 'X-AUTH-TOKEN': data } })
             .then(({ data }) => {
-              context.commit('setUser', data)
-            })
+              context.commit('setUser', data);
+            });
         })
         .catch((err) => {
-          console.error(err)
-        })
+          console.error(err);
+        });
     },
   },
-  modules: {},
-})
+  plugins: [createPersistedState()],
+});
