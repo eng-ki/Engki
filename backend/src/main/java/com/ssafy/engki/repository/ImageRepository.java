bda4cc69b70474b7f91fbc6e9b04789f9cdf4434
Hyejin Park <triaina0@gmail.com>
2020-10-08 05:05:38 +0900

    Refactor random query

--- backend/src/main/java/com/ssafy/engki/repository/ImageRepository.java
+++ backend/src/main/java/com/ssafy/engki/repository/ImageRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.engki.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,12 +9,26 @@ import org.springframework.data.jpa.repository.Query;
 import com.ssafy.engki.entity.Image;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
-	@Query(value = "select i from Image i where i.id in ("
-		+ "select iw.id.imageId from ImageWord iw where iw.id.wordId = :wordId "
-		+ "and 1 = (select count(iw2) from ImageWord iw2 where iw2.id.imageId = iw.id.imageId))")
-	List<Image> getOneObjectImagesOfWord(long wordId);
+	@Query(value = "select * from image i "
+		+ "where i.id in ("
+		+ "select iw.image_id from image_word iw where iw.word_id = :wordId) "
+		+ "order by rand() limit 1",
+		nativeQuery = true)
+	Image getRandomByWord(long wordId);
 
-	@Query(value = "select i from Image i where i.id in ("
-		+ "select iw.id.imageId from ImageWord iw where iw.id.wordId = :wordId) ")
-	List<Image> getAllByWordId(long wordId);
+	@Query(value = "select * from image i "
+		+ "where i.id in ("
+		+ "select iw.image_id from image_word iw where iw.word_id = :wordId "
+		+ "and 1 = (select count(*) from image_word iw2 where iw2.image_id = iw.image_id)) "
+		+ "order by rand() limit 1",
+		nativeQuery = true)
+	Optional<Image> getRandomOneObjectByWord(long wordId);
+
+	@Query(value = "select * from image i "
+		+ "where i.id in ("
+		+ "select iw.image_id from image_word iw where iw.word_id = :wordId "
+		+ "and 1 = (select count(*) from image_word iw2 where iw2.image_id = iw.image_id)) "
+		+ "order by rand() limit :num",
+		nativeQuery = true)
+	List<Image> getRandomListOneObjectByWord(long wordId, int num);
 }
--- backend/src/main/java/com/ssafy/engki/service/CustomEduTestService.java
+++ backend/src/main/java/com/ssafy/engki/service/CustomEduTestService.java
@@ -5,7 +5,6 @@ import static com.ssafy.engki.service.Util.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 import org.springframework.stereotype.Service;
 
@@ -16,23 +15,16 @@ import com.ssafy.engki.entity.CustomImageCaption;
 import com.ssafy.engki.entity.CustomImageCaptionId;
 import com.ssafy.engki.entity.CustomImageWord;
 import com.ssafy.engki.entity.CustomImageWordId;
-import com.ssafy.engki.entity.ImageWord;
 import com.ssafy.engki.entity.Word;
 import com.ssafy.engki.repository.CustomImageCaptionRepository;
-import com.ssafy.engki.repository.CustomImageRepository;
 import com.ssafy.engki.repository.CustomImageWordRepository;
 import com.ssafy.engki.repository.ImageCaptionRepository;
 import com.ssafy.engki.repository.ImageWordRepository;
-import com.ssafy.engki.repository.KidRepository;
-import com.ssafy.engki.repository.ParentRepository;
 import com.ssafy.engki.repository.WordRepository;
 
 @RequiredArgsConstructor
 @Service
 public class CustomEduTestService {
-	private final ParentRepository parentRepository;
-	private final KidRepository kidRepository;
-	private final CustomImageRepository customImageRepository;
 	private final CustomImageWordRepository customImageWordRepository;
 	private final CustomImageCaptionRepository customImageCaptionRepository;
 	private final ImageWordRepository imageWordRepository;
@@ -42,50 +34,43 @@ public class CustomEduTestService {
 	public CustomEduDto.CQuiz2Response getRandomImages(long imageId, String word, long parentId) {
 		CustomEduDto.CQuiz2Response response = new CustomEduDto.CQuiz2Response();
 
-		CustomImageWord customImageWord = customImageWordRepository.getOne(new CustomImageWordId(imageId, word));
+		// 지정 이미지 추가
+		CustomImageWord customImageWord
+			= customImageWordRepository.getOne(new CustomImageWordId(imageId, word));
 		response.getImages().add(CustomEduDto.CImage.builder()
 			.word(customImageWord.getId().getWord())
 			.wordKor(customImageWord.getWordKor())
 			.filePath(customImageWord.getImage().getFilePath())
 			.build());
 
-		// word의 이미지 최대 3개
-		List<CustomImageWord> wordImages
-			= customImageWordRepository.getImagesOfWordExceptImage(word, parentId, imageId);
-		Util.getRandomNumbers(2, wordImages.size()).forEach(idx ->
-			response.getImages().add(CustomEduDto.CImage.builder()
-				.word(wordImages.get(idx).getId().getWord())
-				.wordKor(wordImages.get(idx).getWordKor())
-				.filePath(wordImages.get(idx).getImage().getFilePath())
-				.build())
-		);
-		response.setAnswerNum(Math.min(3, wordImages.size() + 1));
+		// word의 이미지 최대 2개
+		customImageWordRepository.getRandomListByWordExceptImage(word, parentId, imageId, 2)
+			.forEach(wordImage ->
+				response.getImages().add(CustomEduDto.CImage.builder()
+					.word(wordImage.getId().getWord())
+					.wordKor(wordImage.getWordKor())
+					.filePath(wordImage.getImage().getFilePath())
+					.build()));
+		response.setAnswerNum(Math.min(3, response.getImages().size()));
 
 		// word가 아닌 이미지 (6 - word 이미지 개수) 개
-		List<ImageWord> notWordImages = imageWordRepository.getImagesNotWord(word);
-		Util.getRandomNumbers(6 - response.getAnswerNum(), notWordImages.size()).forEach(idx ->
-			response.getImages().add(CustomEduDto.CImage.builder()
-				.word(notWordImages.get(idx).getWord().getWord())
-				.wordKor(notWordImages.get(idx).getWord().getWordKor())
-				.filePath(notWordImages.get(idx).getImage().getFilePath())
-				.build()
-			)
-		);
+		imageWordRepository.getRandomListExceptWord(word, 6 - response.getAnswerNum())
+			.forEach(notWordImage ->
+				response.getImages().add(
+					CustomEduDto.CImage.builder()
+						.word(notWordImage.getWord().getWord())
+						.wordKor(notWordImage.getWord().getWordKor())
+						.filePath(notWordImage.getImage().getFilePath())
+						.build()));
 
 		Collections.shuffle(response.getImages());
-
-		// -> 6개 리턴
 		return response;
 	}
 
 	public CustomEduDto.Segmentation getSegmentation(long imageId, String word) {
-		Random rand = new Random(System.currentTimeMillis());
-
 		CustomImageWord customImageWord
 			= customImageWordRepository.getOne(new CustomImageWordId(imageId, word));
-
-		List<Word> randomWords = wordRepository.getWordsExceptWord(word);
-		Word randomWord = randomWords.get(rand.nextInt(randomWords.size()));
+		Word randomWord = wordRepository.getRandomExceptWord(word);
 
 		return CustomEduDto.Segmentation.builder()
 			.filePath(customImageWord.getImage().getFilePath())
@@ -100,20 +85,17 @@ public class CustomEduTestService {
 			.orElse(customImageCaptionRepository.getOne(imageId));
 
 		List<String> randomWords = new ArrayList<>(Collections.singletonList(customImageCaption.getId().getWord()));
-		List<Word> wordsExceptWord = wordRepository.getWordsExceptWord(customImageCaption.getId().getWord());
-		Util.getRandomNumbers(3, wordsExceptWord.size()).forEach(idx ->
-			randomWords.add(wordsExceptWord.get(idx).getWord())
-		);
-		Collections.shuffle(randomWords);
+		wordRepository.getRandomListExceptWord(word, 3)
+			.forEach(randomWord ->
+				randomWords.add(randomWord.getWord()));
 
-		List<String> captionsExceptWord
-			= imageCaptionRepository.getWordsExceptWord(customImageCaption.getId().getWord());
 		List<String> randomCaptions = new ArrayList<>(Collections.singletonList(customImageCaption.getCaption()));
-		Util.getRandomNumbers(3, captionsExceptWord.size()).forEach(idx ->
-			randomCaptions.add(captionsExceptWord.get(idx))
-		);
-		Collections.shuffle(randomCaptions);
+		imageCaptionRepository.getRandomListExceptWord(word, 3)
+			.forEach(caption ->
+				randomCaptions.add(caption.getCaption()));
 
+		Collections.shuffle(randomWords);
+		Collections.shuffle(randomCaptions);
 		return CustomEduDto.Caption.builder()
 			.filePath(customImageCaption.getImage().getFilePath())
 			.caption(customImageCaption.getCaption())
